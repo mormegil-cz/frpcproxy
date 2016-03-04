899a4ed035790174b8d25970e35fe94e938d380c
Mormegil <mormegil@centrum.cz>
2016-03-04 16:02:23 +0100

    +Example config file

---
+++ dist/config-example.js
@@ -0,0 +1,10 @@
+module.exports = {
+    endpoints: {
+        'endpointname': {
+            hostname: 'upstream.hostname.example',
+            port: 80,
+            path: '/server/path',
+            method: 'calledRpcMethodName'
+        }
+    }
+};
